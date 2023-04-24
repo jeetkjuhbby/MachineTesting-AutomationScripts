11cd0bfc536898e5b5b9f37a81e51aa8f347b7fd
jeetkjuhbby <37928359+jeetkjuhbby@users.noreply.github.com>
2023-04-25 02:24:05 +0530

    Update AddCustomerScript.java

--- src/test/java/com/TestScripts/AddCustomerScript.java
+++ src/test/java/com/TestScripts/AddCustomerScript.java
@@ -8,66 +8,90 @@
 package com.TestScripts;
 
 import java.time.Duration;
+import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AddCustomerScript {
-	
-	public WebDriver driver;
-	ChromeOptions capability;
-	
+	WebDriver driver;
+	WebDriverWait wait;
+
+	// @Parameters({"browserName"})
 	@BeforeMethod
-	public void OpenBrowser() {
+	public void oppenApp() {
 		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.get("https://testffc.nimapinfotech.com/auth/login");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://testffc.nimapinfotech.com/auth/register");
-		 
-		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 	}
-	
-	@Test
-	public void AddCustomer() throws InterruptedException {
-		driver.findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys("Jitesh");
-		driver.findElement(By.xpath("//input[@formcontrolname='mobileno']")).sendKeys("9617408555");
-		driver.findElement(By.xpath("//input[@formcontrolname='emailid']")).sendKeys("jshankhpal42@gmail.com");
-		Thread.sleep(8000);
-		driver.findElement(By.id("kt_login_signin_submit")).click();
-		Thread.sleep(8000);
-		driver.findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys("admin12345");
-		driver.findElement(By.xpath("//input[@formcontrolname='confirmPassword']")).sendKeys("admin12345");
-		driver.findElement(By.id("kt_login_signin_submit")).click();
-		
-		WebElement errorMessage = driver.findElement(By.xpath("//div[text()='Please Enter Correct OTP']"));
-		
-		String actualErrorMessage=errorMessage.getText();
-		
-		if(actualErrorMessage.endsWith("Please Enter Correct OTP")) {
-			System.out.println("Sign-up failed");
-		}
-		else {
-			System.out.println("Sign-up successfull");
-		}
-		
-		
-		
+
+	@Test(priority = 1)
+	public void login() throws InterruptedException {
+		WebElement id = driver.findElement(By.xpath("//input[@formcontrolname='username']"));
+		wait.until(ExpectedConditions.visibilityOf(id)).sendKeys("jshankhpal40@gmail.com");
+		WebElement password = driver.findElement(By.xpath("//input[@formcontrolname='password']"));
+		wait.until(ExpectedConditions.visibilityOf(password)).sendKeys("jitesh0987");
+		WebElement loginButton = driver.findElement(By.id("kt_login_signin_submit"));
+		String str = JOptionPane.showInputDialog("Enter Your captcha");
+		WebElement captcha = driver.findElement(By.xpath("//input[@formcontrolname='captchaValue']"));
+		captcha.sendKeys(str);
+		loginButton.click();
+		Thread.sleep(5000);
+
 	}
+
+	@AfterMethod
+	public void addCustomer() throws InterruptedException {
+		driver.findElement(By.xpath("//span[text()='My Customers']")).click();
+		driver.findElement(By.xpath("//span[text()=' New Customer ']")).click();
+		driver.findElement(By.xpath("//input[@formcontrolname='LeadName']")).sendKeys("nimapInfotech");
+		driver.findElement(By.xpath("//input[@formcontrolname='RefNo']")).sendKeys("10023");
+		driver.findElement(By.xpath("//input[@formcontrolname='PersonName']")).sendKeys("Jitesh");
+		driver.findElement(By.xpath("//input[@formcontrolname='MobileNo']")).sendKeys("9637408888");
+		driver.findElement(By.xpath("//input[@formcontrolname='Email']")).sendKeys("Jitesh@gmail.com");
+		driver.findElement(By.xpath("//input[@formcontrolname='PersonDesignation']")).sendKeys("Tester");
+
+		driver.findElement(By.xpath("(//div[@class='mat-select-value'])[15]")).click();
+		List<WebElement> allCountries = driver
+				.findElements(By.xpath("//mat-option[@class='mat-option ng-star-inserted']"));
+
+		System.out.println(allCountries.size());
+
+		for (WebElement country : allCountries) {
+			if (country.getText().equals("India")) {
+				country.click();
+			}
+		}
+
+		driver.findElement(By.xpath("//div[@class='mat-checkbox-inner-container']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("(//button[@class='mat-raised-button mat-button-base mat-primary'])[4]")).click();
+
+		driver.findElement(By.xpath("//span[text()=' Save ']")).click();
+		Thread.sleep(5000);
+
+		WebElement accCreated = driver
+				.findElement(By.xpath("//div[@class='toast-top-right toast-container']"));
+
+		System.out.println(accCreated.getText());
 		
-		@AfterMethod
-		public void CloseBrowser() {
-			//driver.close();
-			
-	
+
 	}
 
 }
